c20064e4d6606128f1e26ba536d5afecb46d85cb
rockypurba <70028246+rockypurba@users.noreply.github.com>
2020-09-18 07:56:35 -0700

    Add files via upload

---
+++ ExpressionManager.cpp
@@ -0,0 +1,21 @@
+#include "ExpressionManager.h"
+
+bool ExpressionManager::isBalanced(string expression){
+	cout << "isBalanced: " << expression << endl;
+	return true; //REPLACE
+}
+
+string ExpressionManager::postfixToInfix(string postfixExpression){
+	cout << "postfixToInfix: " << postfixExpression << endl;
+	return "REPLACE";
+}
+
+string ExpressionManager::postfixEvaluate(string postfixExpression){
+	cout << "postfixEvaluate: " << postfixExpression << endl;
+	return "REPLACE";
+}
+
+string ExpressionManager::infixToPostfix(string infixExpression){
+	cout << "infixToPostfix: " << infixExpression << endl;
+	return "REPLACE";
+}
